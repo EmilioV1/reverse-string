f301c4551609f6eaaf9c13f478f5365ad8812e2a
Emilio Valladares <vallae.wumbo@gmail.com>
2020-01-19 14:11:53 -0800

    Added method for inserting into new array after decrementing through original string. Complete!

--- ReverseString.java
+++ ReverseString.java
@@ -7,17 +7,25 @@
  */
 public class ReverseString {
     
-    public static void main(String args[]){
+    public static void main(String args[]) {
         String e = reverse("Emilio");
         System.out.println(e);
     }
     
-    public static String reverse(String e){
+    public static String reverse(String e) {
         char [] letters = new char [e.length()];
         
-        for(int i = e.length()-1; i >= 0; i--){
-            System.out.println(e.charAt(i));
+        int letterIndex = 0;
+        for(int i = e.length()-1; i >= 0; i--) {
+            letters[letterIndex] = e.charAt(i);
+            letterIndex++;
         }
-        return e;
+        
+        String reverse = "";
+        for(int i = 0; i < e.length(); i++) {
+            reverse = reverse + letters[i];  
+        }
+        
+        return reverse;
     }
 }
